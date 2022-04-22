04897afcb4dc862c2f8c3915d0c420ec0678431f
mpioski <matheus.pioski@gmail.com>
2022-04-21 21:31:49 -0300

    Adicionado criação do path de download por default

--- crawler.py
+++ crawler.py
@@ -1,9 +1,19 @@
+import os
+
 from driver.chromium import Chromium
 
 
 class Crawler:
+
     def __init__(self, url: str, download_path: str, debug: bool):
         self.url = url
+        self.download_path = download_path
+        if not self.download_path:
+            current_path = os.path.abspath(os.getcwd())
+            manga_name = url.split("/")[4]
+            self.download_path = os.path.join(current_path, manga_name)
+            os.makedirs(self.download_path, exist_ok=True)
+
         self.download_path = download_path
         self.debug = debug
 
@@ -11,4 +21,3 @@ class Crawler:
         with Chromium(debug=self.debug) as chromium:
             driver = chromium.driver
             driver.get(self.url)
-
